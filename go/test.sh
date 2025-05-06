3bbca0ad7bf99525fa45477354e5978e46f50d4b
Sharon Aicler <saichler@gmail.com>
2025-05-06 14:12:43 -0700

    switch to security loader

--- go/test.sh
+++ go/test.sh
@@ -11,9 +11,17 @@ rm -rf vendor
 go mod init
 GOPROXY=direct GOPRIVATE=github.com go mod tidy
 go mod vendor
+cp ./vendor/github.com/saichler/shared/go/share/resources/build-test-security.sh .
+chmod +x ./build-test-security.sh
+rm -rf vendor
+./build-test-security.sh
+rm -rf ./build-test-security.sh
+go mod vendor
 
 # Run unit tests with coverage
 go test -tags=unit -v -coverpkg=./gsql/... -coverprofile=cover.html ./... --failfast
+
+rm -rf ./tests/loader.so
 
 # Open the coverage report in a browser
 go tool cover -html=cover.html
