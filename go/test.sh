ba445f1f62bb9c2550313ce3b73b6b76fc9b3f22
Sharon Aicler <saichler@gmail.com>
2025-05-28 06:58:56 -0700

    Fix test run script

--- go/test.sh
+++ go/test.sh
@@ -11,7 +11,8 @@ rm -rf vendor
 go mod init
 GOPROXY=direct GOPRIVATE=github.com go mod tidy
 go mod vendor
-cp ./vendor/github.com/saichler/shared/go/share/resources/build-test-security.sh .
+
+cp ./vendor/github.com/saichler/l8utils/go/utils/resources/build-test-security.sh .
 chmod +x ./build-test-security.sh
 rm -rf vendor
 ./build-test-security.sh
